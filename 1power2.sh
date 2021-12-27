59db108a9d98fb40a7008ff99748a5bb48e9abcf
Shubham <nhaldes@gmail.com>
2021-12-28 00:10:48 +0530

    add 1power2.sh

--- 1power2.sh
+++ 1power2.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x
 
-read -p  "Enter any power of 2 (in number) : " n
+read -p "enter a number : " n
 
-for((count=0; count<=$n; count++))
-  do
-      a=$((2**$count))
-      echo $a
-  done
+a=1
+
+	for ((count=1; count<=$n; count++))
+do
+
+	a=$((2**count))
+if [ $a -lt 256 ]
+	then
+echo $a
+	fi
+done
